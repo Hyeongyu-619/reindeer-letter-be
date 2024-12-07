e58cb245a552867a3a4e344b405b138e8142e16e
Hyeongyu-619 <rlagusrb619@gmail.com>
2024-12-07 13:58:50 +0900

    Feat: 모듈 설치 및 앱 모듈 의존성 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,13 +2,12 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
-import { AuthController } from './auth/auth.controller';
 import { AuthModule } from './auth/auth.module';
-import { AuthService } from './auth/auth.service';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [PrismaModule, AuthModule],
-  controllers: [AppController, AuthController],
-  providers: [AppService, AuthService],
+  imports: [PrismaModule, AuthModule, UsersModule],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
