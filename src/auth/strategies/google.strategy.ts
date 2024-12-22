ac1208a2836151db810b164961c9fea91a90aae6
hyeongyu-619 <rlagusrb619@gmail.com>
2024-12-22 17:40:55 +0900

    1222

--- src/auth/strategies/google.strategy.ts
+++ src/auth/strategies/google.strategy.ts
@@ -5,23 +5,32 @@ import { AuthService } from '../auth.service';
 import * as devConfig from '../../../dev.json';
 
 @Injectable()
-export class GoogleStrategy extends PassportStrategy(Strategy, 'google') {
+export class GoogleStrategy extends PassportStrategy(Strategy) {
   constructor(private authService: AuthService) {
     super({
       clientID: devConfig.GOOGLE_CLIENT_ID,
       clientSecret: devConfig.GOOGLE_CLIENT_SECRET,
       callbackURL: devConfig.GOOGLE_CALLBACK_URL,
       scope: ['email', 'profile'],
+      passReqToCallback: true,
     });
   }
 
-  async validate(accessToken: string, refreshToken: string, profile) {
-    const { name, emails } = profile;
+  async validate(
+    req: any,
+    accessToken: string,
+    refreshToken: string,
+    profile: any,
+  ) {
+    const { id, emails, displayName } = profile;
+    const email = emails[0].value;
+    const code = req.query.code;
+
     const user = await this.authService.findOrCreateGoogleUser({
-      googleId: profile.id,
-      email: emails[0].value,
-      nickname: name.givenName,
-      code: null,
+      googleId: id,
+      email,
+      nickname: displayName || `사용자${id}`,
+      code,
     });
 
     return user;
